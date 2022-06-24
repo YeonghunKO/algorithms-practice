6d9884dcbba5a186d948e8eb14a551ca0958af77
Sinkyo <yhko1988@gmail.com>
2022-06-24 23:46:11 +0900

    map 자료구조로 알고리즘 완성!

---
+++ 자료구조/filter_anagram.js
@@ -0,0 +1,12 @@
+function aclean(arr) {
+  let map = new Map();
+  arr.forEach(str => {
+    let sortedStr = str.toLowerCase().split('').sort().join('');
+    map.set(sortedStr, str);
+  });
+  return [...map.values()];
+}
+
+let arr = ['nap', 'teachers', 'cheaters', 'PAN', 'ear', 'era', 'hectares'];
+
+console.log(aclean(arr));
