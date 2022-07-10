13c786376dba2bfa219b24329fb620a777d872a2
Sinkyo <yhko1988@gmail.com>
2022-07-10 19:58:45 +0900

    소수찾기 끝! return을 하면 안되는 거였음..

--- 알고리즘/소수찾기/findPrime.js
+++ 알고리즘/소수찾기/findPrime.js
@@ -5,7 +5,7 @@ function findPrime(num) {
   combi(numArr, '');
 
   function isPrime(num) {
-    for (let i = 2; Math.sqrt(num); i++) {
+    for (let i = 2; i < Math.sqrt(num); i++) {
       if (num % i === 0) {
         return false;
       }
@@ -16,20 +16,21 @@ function findPrime(num) {
 
   function combi(arr, str) {
     if (str.length) {
-      if (!set.has(str)) {
-        if (isPrime(str)) {
-          set.add(str);
-          return;
+      if (!set.has(Number(str))) {
+        if (isPrime(Number(str))) {
+          set.add(Number(str));
+          console.log(str);
+          // return 을 여기서 해버리면 아래로 넘어가지 않아서 array가 중간에서 멈춘다...
+          // 이것때문에 거의 1시간을 헤맸는데 그래도 버그를 찾아서 뿌듯하다.
         }
       }
     }
 
     if (arr.length) {
       for (let i = 0; i < arr.length; i++) {
-        const subArr = arr.slice();
-        const subStr = subArr[i];
+        const subArr = arr.slice(0);
         subArr.splice(i, 1);
-        combi(subArr, str + subStr);
+        combi(subArr, str + arr[i]);
       }
     }
   }
@@ -37,4 +38,4 @@ function findPrime(num) {
   console.log(set);
 }
 
-findPrime('137');
+findPrime('123');
